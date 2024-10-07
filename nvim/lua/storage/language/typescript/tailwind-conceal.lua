ff9e735c81001ba833014bab6fd57edc65c6e3a7
pantorn <longpantorn@gmail.com>
2024-10-07 19:57:47 +0700

    fix nvim

--- nvim/lua/storage/language/typescript/tailwind-conceal.lua
+++ nvim/lua/storage/language/typescript/tailwind-conceal.lua
@@ -1,18 +1,24 @@
-
 return {
-  'razak17/tailwind-fold.nvim',
-  keys = { {
-    '<leader>tw',
-    '<cmd>TailwindFoldToggle<cr>',
-    desc = 'Toggle Tailwind Fold',
-  } },
-  opts = {
-    enabled = true,
-    symbol = '…', -- 󱏿
-    highlight = {
-      fg = '#38BDF8', -- [[ symbol color ]]
+  "razak17/tailwind-fold.nvim",
+  keys = {
+    {
+      "<leader>tw",
+      "<cmd>TailwindFoldToggle<cr>",
+      desc = "Toggle Tailwind Fold",
+    },
+    {
+      mode = { "n", "t", "i", "x" },
+      "<leader>tf",
+      false,
     },
   },
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  opts = {
+    enabled = true,
+    symbol = "…", -- 󱏿
+    highlight = {
+      fg = "#38BDF8", -- [[ symbol color ]]
+    },
+  },
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
   -- ft = { 'html', 'svelte', 'astro', 'vue', 'typescriptreact', 'javascriptreact', 'php', 'blade' },
 }
