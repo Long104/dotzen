e8d7bdd27a838166b6a50c83524bb0fb4f9e9141
pantorn <longpantorn@gmail.com>
2024-11-25 22:40:42 +0700

    add spicetify

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -2,7 +2,8 @@
   description = "My Darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     # nix-darwin.url = "github:LnL7/nix-darwin";
     # nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
