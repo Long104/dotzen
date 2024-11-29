bdf442a04d015ff61fdb6bccc617596b51c3a174
pantorn <longpantorn@gmail.com>
2024-11-29 14:22:19 +0700

    add action preview and tiny

--- nvim/lua/plugins/actions-preview.lua
+++ nvim/lua/plugins/actions-preview.lua
@@ -3,6 +3,6 @@ return {
   "aznhe21/actions-preview.nvim",
   config = function()
     require("actions-preview").setup()
-    vim.keymap.set({ "v", "n" }, "gf", require("actions-preview").code_actions)
+    vim.keymap.set({ "v", "n" }, "<leader>ap", require("actions-preview").code_actions)
   end,
 }
