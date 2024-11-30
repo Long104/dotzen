5af40d5e8c722380768d285bbc8910ee9f30e7ee
pantorn <longpantorn@gmail.com>
2024-11-30 14:43:51 +0700

    delete grammary

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -16,10 +16,6 @@
   nixpkgs.config = {
     allowUnfree = true;
   };
-  programs.java = {
-    enable = true;
-    package = pkgs.oraclejre8;
-  };
 
   home.username = "pantornchuavallee";
   home.homeDirectory = "/Users/pantornchuavallee";
