32942cb41079ca073c848cae14c1ce0365d5e022
pantorn <longpantorn@gmail.com>
2024-12-05 16:29:03 +0700

    add container docker plugin

--- nvim/lua/plugins/utility/devops/container.lua
+++ nvim/lua/plugins/utility/devops/container.lua
@@ -2,4 +2,13 @@ return {
   "dgrbrady/nvim-docker",
   dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
   rocks = "4O4/reactivex", -- ReactiveX Lua implementation
+  config = function()
+    local nvim_docker = require "nvim-docker"
+    -- list containers
+    nvim_docker.containers.list_containers()
+
+    -- OR
+    -- setting up keybindings since the `list_containers` fn does not have a default binding
+    vim.keymap.set("n", "<leader>C", nvim_docker.containers.list_containers)
+  end,
 }
--- nvim/lua/plugins/utility/devops/docker.lua
+++ nvim/lua/plugins/utility/devops/docker.lua
@@ -1,32 +1,31 @@
-
 return {
-  'krisajenkins/telescope-docker.nvim',
-  event = 'VeryLazy',
+  "krisajenkins/telescope-docker.nvim",
+  -- event = 'VeryLazy',
+  keys = {
+    {
+      "<Leader>dv",
+      ":Telescope telescope_docker docker_volumes<CR>",
+      desc = "[D]ocker [V]olumes",
+    },
+    {
+      "<Leader>di",
+      ":Telescope telescope_docker docker_images<CR>",
+      desc = "[D]ocker [I]mages",
+    },
+    {
+      "<Leader>dp",
+      ":Telescope telescope_docker docker_ps<CR>",
+      desc = "[D]ocker [P]rocesses",
+    },
+  },
   dependencies = {
-    'nvim-telescope/telescope.nvim',
+    "nvim-telescope/telescope.nvim",
   },
   config = function()
-    require('telescope').load_extension 'telescope_docker'
-    require('telescope_docker').setup {}
+    require("telescope").load_extension "telescope_docker"
+    require("telescope_docker").setup {}
   end,
 
   -- Example keybindings. Adjust these to suit your preferences or remove
   --   them entirely:
-  keys = {
-    {
-      '<Leader>dv',
-      ':Telescope telescope_docker docker_volumes<CR>',
-      desc = '[D]ocker [V]olumes',
-    },
-    {
-      '<Leader>di',
-      ':Telescope telescope_docker docker_images<CR>',
-      desc = '[D]ocker [I]mages',
-    },
-    {
-      '<Leader>dp',
-      ':Telescope telescope_docker docker_ps<CR>',
-      desc = '[D]ocker [P]rocesses',
-    },
-  },
 }
