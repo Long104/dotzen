b2407434f5bca1e7d263409ae7b6da31c19b2906
pantorn <longpantorn@gmail.com>
2024-12-06 16:49:59 +0700

    add typr

---
+++ nvim/lua/storage/morePlugin/nvzone/minty.lua
@@ -0,0 +1,9 @@
+return {
+  { "nvzone/volt", lazy = true },
+  {
+    "nvzone/minty",
+    cmd = { "Shades", "Huefy" },
+  },
+  { "nvzone/timerly", cmd = "TimerlyToggle" },
+  { "nvzone/showkeys", cmd = "ShowkeysToggle" },
+}
